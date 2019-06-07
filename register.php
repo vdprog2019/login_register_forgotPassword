232cff7647c28ce7814d8b3629cc71a04dde0f3b
vdprog2019 <48090405+vdprog2019@users.noreply.github.com>
2019-06-07 13:24:48 +0300

    добавил отправка почты

--- register.php
+++ register.php
@@ -1,2 +1,32 @@
 <?php
+ini_set( 'display_errors', 1 );
+
+error_reporting( E_ALL );
+
+$from = "vdphoto77@gmail.com";
+
+$to = "$_GET[email_php]"; //надо ввести email
+
+$subject = "Checking PHP mail";
+
+$message = "PHP mail works just fine";
+
+$headers = "From:" . $from;
+
+mail($to,$subject,$message, $headers);
+
+echo "The email message was sent.";
+
+
+$e = $_GET["email_php"];
+$p = $_GET["password_php"];
+$s = $_GET["second_password_php"];
+
+echo "<table>
+    <tr><td>email</td><td>$e</td></tr> 
+    <tr><td>password</td><td>$p</td></tr> 
+    <tr><td>second password</td><td>$s</td></tr> 
+</table>
+";
+
 
